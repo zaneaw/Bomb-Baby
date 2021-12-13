7240a0995c4ebe9a0b2d0cc859d68af54532a683
Zane Wilson <wilsonzanea@gmail.com>
2021-12-13 18:22:48 -0500

    minor tweaks

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def print_decorator(func_to_dec):
                 print(f"It will only be {results[1]} generation(s) until we can destroy the LAMBCHOP doomsday device!!!!")
             else: print("But... We are DOOMED! It is IMPOSSIBLE...")
         else:
-            print(f"UH OH!!!\n{results[0]} results were not correct! We were expecting {self.answer}, but {results[1]} was the intended result of the function.")
+            print(f"UH OH!!!\n{results[0]} results were not correct! We were expecting '{obj.answer}', but got '{results[1]}' as a result of the function.")
     return wrapper
 
 
@@ -23,27 +23,23 @@ class Test():
         self.x = x
         self.y = y
         self.answer = answer
-        self.b4()
+        self.bb()
 
     @print_decorator
-    def b4(self):
-        test = "Bomb Baby 4"
-        result = bb4.solution(self.x, self.y)
+    def bb(self):
+        test = "Bomb Baby"
+        result = bombbaby.solution(self.x, self.y)
         return test, result
-
-    # @print_decorator
-    # def b3(self):
-    #     test = "Bomb Baby 3"
-    #     result = bb3.solution(self.x, self.y)
-    #     return test, result, self.name
 
 
 def int_gen(): 
     return random.randint(1, 10**50)
 
+x = int_gen()
 
-test_1 = Test('2', '1', '1')
-test_2 = Test('4', '7', '4')
-test_3 = Test('2', '4', 'impossible')
-test_4 = Test(int_gen(), int_gen(), 'test')
+test1 = Test('2', '1', '1')
+test2 = Test('4', '7', '4')
+test3 = Test('2', '4', 'impossible')
+test4 = Test('2591', '9152', '26')
+# test5 = Test(x, x, 'test')
 
